539b256d50bbc56cffe3d174fdf86ca02c12b3df
Thomas Paul <paul.xx9>
2014-04-02 17:39:15 +0200

    first commit

---
+++ NokiaPhoneSimulator/src/com/example/nokiaphonesimulator/NokiaPhoneActivity.java
@@ -0,0 +1,28 @@
+package com.example.nokiaphonesimulator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see SystemUiHider
+ */
+public class NokiaPhoneActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_nokia_phone);
+    }
+
+}
