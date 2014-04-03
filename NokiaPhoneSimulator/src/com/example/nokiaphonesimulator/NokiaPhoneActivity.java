96022544daa8ce452c14ce0fb1ad842bba1a2c48
Thomas Paul <paul.xx9>
2014-04-03 22:09:12 +0200

    added some button sounds

--- NokiaPhoneSimulator/src/com/example/nokiaphonesimulator/NokiaPhoneActivity.java
+++ NokiaPhoneSimulator/src/com/example/nokiaphonesimulator/NokiaPhoneActivity.java
@@ -1,7 +1,11 @@
 package com.example.nokiaphonesimulator;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -11,16 +15,12 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
 public class NokiaPhoneActivity extends Activity {
 	
-	String phone_number = "";
-	TextView output1;	
+	private String phone_number = "";
+	private TextView output1;	
+	
+	private MediaPlayer mp;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,55 +53,67 @@ public class NokiaPhoneActivity extends Activity {
 		Button btn_up = (Button) this.findViewById(R.id.btn_up);
 		
 		output1 = (TextView) this.findViewById(R.id.textView_output1);
+
+		mp = new MediaPlayer();
 		
 		btn_zero.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				digitButton("0");
+				playSound("cell-phone-0.mp3");
 			}});
 		
 		btn_one.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				digitButton("1");
+				playSound("cell-phone-1.mp3");
 			}});
 		
 		btn_two.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				digitButton("2");
+				playSound("cell-phone-2.mp3");
 			}});
 		
 		btn_three.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				digitButton("3");
+				playSound("cell-phone-3.mp3");
 			}});
 		
 		btn_four.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				digitButton("4");
+				playSound("cell-phone-4.mp3");
 			}});
 		
 		btn_five.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				digitButton("5");
+				playSound("cell-phone-5.mp3");
 			}});
 		
 		btn_six.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				digitButton("6");
+				playSound("cell-phone-6.mp3");
 			}});
 		
 		btn_seven.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				digitButton("7");
+				playSound("cell-phone-7.mp3");
 			}});
 		
 		btn_eight.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				digitButton("8");
+				playSound("cell-phone-8.mp3");
 			}});
 		
 		btn_nine.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				digitButton("9");
+				playSound("cell-phone-9.mp3");
 			}});
 		
 		btn_star.setOnClickListener(new OnClickListener() {
@@ -143,6 +155,24 @@ public class NokiaPhoneActivity extends Activity {
 		phone_number += digit;
 		output1.setText(phone_number);
 	}
+    
+    private void playSound(String sound_name){
+    	 if(mp.isPlaying())  
+             mp.stop();
+
+         mp.reset();
+         AssetFileDescriptor afd;
+    	 try {
+             afd = getAssets().openFd(sound_name);
+             mp.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+             mp.prepare();
+             mp.start();
+         } catch (IllegalStateException e) {
+             e.printStackTrace();
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+    }
     
     private void call(String phone_number) {
             Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phone_number));
