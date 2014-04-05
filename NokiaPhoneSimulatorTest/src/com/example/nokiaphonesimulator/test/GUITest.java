7eb0398b1693f342a8e147813679114623874472
Thomas Paul <paul.xx9>
2014-04-05 16:23:55 +0200

    [VP,TP] added feedback to btn_two

--- NokiaPhoneSimulatorTest/src/com/example/nokiaphonesimulator/test/GUITest.java
+++ NokiaPhoneSimulatorTest/src/com/example/nokiaphonesimulator/test/GUITest.java
@@ -24,22 +24,22 @@ public class GUITest extends
 	}
 
 	public void testNumbers() {
-		this.solo.clickOnButton("enter");
-		this.solo.clickOnButton("*");
-		this.solo.clickOnButton("0");
-		this.solo.clickOnButton("#");
-		this.solo.clickOnButton("7");
-		this.solo.clickOnButton("8");
-		this.solo.clickOnButton("9");
-		this.solo.clickOnButton("4");
-		this.solo.clickOnButton("5");
-		this.solo.clickOnButton("6");
-		this.solo.clickOnButton("1");
-		this.solo.clickOnButton("2");
-		this.solo.clickOnButton("3");
-		this.solo.clickOnButton("clear");
-		this.solo.clickOnButton("down");
-		this.solo.clickOnButton("up");
+		this.solo.clickOnButton(0);
+		//this.solo.clickOnButton(1);
+		this.solo.clickOnButton(2);
+		this.solo.clickOnButton(3);
+		this.solo.clickOnButton(4);
+		this.solo.clickOnButton(5);
+		this.solo.clickOnButton(6);
+		this.solo.clickOnButton(7);
+		this.solo.clickOnButton(8);
+		this.solo.clickOnButton(9);
+		this.solo.clickOnButton(10);
+		this.solo.clickOnButton(11);
+		this.solo.clickOnButton(12);
+		this.solo.clickOnButton(13);
+		this.solo.clickOnButton(14);
+		this.solo.clickOnButton(15);
 
 	}
 
