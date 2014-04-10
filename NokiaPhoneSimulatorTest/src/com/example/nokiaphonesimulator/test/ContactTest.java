c6a741acc79533663a39a7bf277f8b701c805686
Thomas Paul <paul.xx9>
2014-04-10 14:42:31 +0200

    [VP,TP] finished ContactTest.class

--- NokiaPhoneSimulatorTest/src/com/example/nokiaphonesimulator/test/ContactTest.java
+++ NokiaPhoneSimulatorTest/src/com/example/nokiaphonesimulator/test/ContactTest.java
@@ -18,5 +18,20 @@ public class ContactTest extends TestCase
 	    assertEquals("given_name must be emtpy", "", default_contact.getGivenName());
 	    assertEquals("family_name must be emtpy", "", default_contact.getFamilyName());
 	    assertEquals("phone_number must be emtpy", "", default_contact.getPhoneNumber());
+	}
+	
+	public void testConstructor() 
+	{
+		String id = "321";
+		String given_name = "Max";
+		String family_name = "Jedermann";
+		String phone_number = "1234567890";
+		
+        Contact contact = new Contact(id, given_name, family_name, phone_number);
+        
+	    assertEquals("id must be 321", id, contact.getId());
+	    assertEquals("given_name must be Max", given_name, contact.getGivenName());
+	    assertEquals("family_name must be Jedermann", family_name, contact.getFamilyName());
+	    assertEquals("phone_number must be 1234567890", phone_number, contact.getPhoneNumber());
 	} 
 }
