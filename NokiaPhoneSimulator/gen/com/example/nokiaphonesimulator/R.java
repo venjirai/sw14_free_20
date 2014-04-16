bc54015f3358843ad2742384782a86bb3b8a1846
venjirai <ventu@gmx.at>
2014-04-16 06:06:22 +0200

    [VP, TP] replaced mediaplayer with soundpool
    We replaced mediaplayer with soundpool which is more suited for our
    application.

--- NokiaPhoneSimulator/gen/com/example/nokiaphonesimulator/R.java
+++ NokiaPhoneSimulator/gen/com/example/nokiaphonesimulator/R.java
@@ -17,8 +17,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int btn_clear=0x7f020000;
@@ -73,57 +73,69 @@ public final class R {
         public static final int nokia_phone=0x7f020031;
     }
     public static final class id {
-        public static final int action_settings=0x7f080017;
-        public static final int btn_clear=0x7f080007;
-        public static final int btn_down=0x7f080008;
-        public static final int btn_eight=0x7f080012;
-        public static final int btn_enter=0x7f080009;
-        public static final int btn_five=0x7f08000f;
-        public static final int btn_four=0x7f08000e;
-        public static final int btn_nine=0x7f080013;
-        public static final int btn_one=0x7f08000b;
-        public static final int btn_pound=0x7f080016;
-        public static final int btn_seven=0x7f080011;
-        public static final int btn_six=0x7f080010;
-        public static final int btn_star=0x7f080014;
-        public static final int btn_three=0x7f08000d;
-        public static final int btn_two=0x7f08000c;
-        public static final int btn_up=0x7f08000a;
-        public static final int btn_zero=0x7f080015;
-        public static final int container=0x7f080000;
-        public static final int contents=0x7f080001;
-        public static final int textView_output1=0x7f080002;
-        public static final int textView_output2=0x7f080003;
-        public static final int textView_output3=0x7f080004;
-        public static final int textView_output4=0x7f080005;
-        public static final int textView_output5=0x7f080006;
+        public static final int action_settings=0x7f090017;
+        public static final int btn_clear=0x7f090007;
+        public static final int btn_down=0x7f090008;
+        public static final int btn_eight=0x7f090012;
+        public static final int btn_enter=0x7f090009;
+        public static final int btn_five=0x7f09000f;
+        public static final int btn_four=0x7f09000e;
+        public static final int btn_nine=0x7f090013;
+        public static final int btn_one=0x7f09000b;
+        public static final int btn_pound=0x7f090016;
+        public static final int btn_seven=0x7f090011;
+        public static final int btn_six=0x7f090010;
+        public static final int btn_star=0x7f090014;
+        public static final int btn_three=0x7f09000d;
+        public static final int btn_two=0x7f09000c;
+        public static final int btn_up=0x7f09000a;
+        public static final int btn_zero=0x7f090015;
+        public static final int container=0x7f090000;
+        public static final int contents=0x7f090001;
+        public static final int textView_output1=0x7f090002;
+        public static final int textView_output2=0x7f090003;
+        public static final int textView_output3=0x7f090004;
+        public static final int textView_output4=0x7f090005;
+        public static final int textView_output5=0x7f090006;
     }
     public static final class layout {
         public static final int activity_nokia_phone=0x7f030000;
     }
     public static final class menu {
-        public static final int nokia_phone=0x7f070000;
+        public static final int nokia_phone=0x7f080000;
+    }
+    public static final class raw {
+        public static final int s0=0x7f040000;
+        public static final int s1=0x7f040001;
+        public static final int s2=0x7f040002;
+        public static final int s3=0x7f040003;
+        public static final int s4=0x7f040004;
+        public static final int s5=0x7f040005;
+        public static final int s6=0x7f040006;
+        public static final int s7=0x7f040007;
+        public static final int s8=0x7f040008;
+        public static final int s9=0x7f040009;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int clear=0x7f05000f;
-        public static final int down=0x7f050012;
-        public static final int dummy=0x7f05000e;
-        public static final int eight=0x7f050009;
-        public static final int enter=0x7f050010;
-        public static final int five=0x7f050006;
-        public static final int four=0x7f050005;
-        public static final int nine=0x7f05000a;
-        public static final int one=0x7f050002;
-        public static final int pound=0x7f05000d;
-        public static final int seven=0x7f050008;
-        public static final int six=0x7f050007;
-        public static final int star=0x7f05000b;
-        public static final int three=0x7f050004;
-        public static final int two=0x7f050003;
-        public static final int up=0x7f050011;
-        public static final int zero=0x7f05000c;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int clear=0x7f06000f;
+        public static final int down=0x7f060012;
+        public static final int dummy=0x7f06000e;
+        public static final int eight=0x7f060009;
+        public static final int enter=0x7f060010;
+        public static final int five=0x7f060006;
+        public static final int four=0x7f060005;
+        public static final int nine=0x7f06000a;
+        public static final int one=0x7f060002;
+        public static final int pound=0x7f06000d;
+        public static final int seven=0x7f060008;
+        public static final int six=0x7f060007;
+        public static final int star=0x7f06000b;
+        public static final int three=0x7f060004;
+        public static final int two=0x7f060003;
+        public static final int up=0x7f060011;
+        public static final int zero=0x7f06000c;
     }
     public static final class style {
         /** 
@@ -147,11 +159,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int NokiaTextView=0x7f060002;
+        public static final int AppTheme=0x7f070001;
+        public static final int NokiaTextView=0x7f070002;
     }
 }
--- NokiaPhoneSimulator/src/com/example/nokiaphonesimulator/NokiaPhoneActivity.java
+++ NokiaPhoneSimulator/src/com/example/nokiaphonesimulator/NokiaPhoneActivity.java
@@ -10,181 +10,254 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class NokiaPhoneActivity extends Activity {
-	
+import android.media.SoundPool;
+import android.media.AudioManager;
+
+
+public class NokiaPhoneActivity extends Activity implements OnClickListener
+{
 	private Context context;
 	
 	private String phone_number = "";
-	private SoundPlayer sound_player;
-	private ContactList contact_list;
+  private SoundPool sp;
+	//private ContactList contact_list;
 	private int cursor;
 		
 	private TextView output1;
 	private TextView output2;
 	private TextView output3;
 	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_nokia_phone);
-        
-        context = this.getApplicationContext();
-        
-        Button btn_zero = (Button) this.findViewById(R.id.btn_zero);
-		Button btn_one = (Button) this.findViewById(R.id.btn_one);
-		Button btn_two = (Button) this.findViewById(R.id.btn_two);
-		Button btn_three = (Button) this.findViewById(R.id.btn_three);
-		Button btn_four = (Button) this.findViewById(R.id.btn_four);
-		Button btn_five = (Button) this.findViewById(R.id.btn_five);
-		Button btn_six = (Button) this.findViewById(R.id.btn_six);
-		Button btn_seven = (Button) this.findViewById(R.id.btn_seven);
-		Button btn_eight = (Button) this.findViewById(R.id.btn_eight);
-		Button btn_nine = (Button) this.findViewById(R.id.btn_nine);
-		
-		Button btn_star = (Button) this.findViewById(R.id.btn_star);
-		Button btn_pound = (Button) this.findViewById(R.id.btn_pound);
-		Button btn_clear = (Button) this.findViewById(R.id.btn_clear);
-		Button btn_enter = (Button) this.findViewById(R.id.btn_enter);
-		Button btn_down = (Button) this.findViewById(R.id.btn_down);
-		Button btn_up = (Button) this.findViewById(R.id.btn_up);
-		
-		output1 = (TextView) this.findViewById(R.id.textView_output1);
-		output2 = (TextView) this.findViewById(R.id.textView_output2);
-		output3 = (TextView) this.findViewById(R.id.textView_output3);
-
-		sound_player = new SoundPlayer(context);
-		contact_list = new ContactList(context);
-		
-		btn_zero.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				digitButton("0");
-				sound_player.play("cell-phone-0.mp3");
-			}});
-		
-		btn_one.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				digitButton("1");
-				sound_player.play("cell-phone-1.mp3");
-			}});
-		
-		btn_two.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				digitButton("2");
-				sound_player.play("cell-phone-2.mp3");
-			}});
-		
-		btn_three.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				digitButton("3");
-				sound_player.play("cell-phone-3.mp3");
-			}});
-		
-		btn_four.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				digitButton("4");
-				sound_player.play("cell-phone-4.mp3");
-			}});
-		
-		btn_five.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				digitButton("5");
-				sound_player.play("cell-phone-5.mp3");
-			}});
-		
-		btn_six.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				digitButton("6");
-				sound_player.play("cell-phone-6.mp3");
-			}});
-		
-		btn_seven.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				digitButton("7");
-				sound_player.play("cell-phone-7.mp3");
-			}});
-		
-		btn_eight.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				digitButton("8");
-				sound_player.play("cell-phone-8.mp3");
-			}});
-		
-		btn_nine.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				digitButton("9");
-				sound_player.play("cell-phone-9.mp3");
-			}});
-		
-		btn_star.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				
-			}});
-		
-		btn_pound.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				
-			}});
-		
-		btn_clear.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				if(phone_number.length() > 0){
-					phone_number = phone_number.substring(0, phone_number.length() - 1);
-					output1.setText(phone_number);
-				}
-			}});
-		
-		btn_enter.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				call(phone_number);
-			}});
-		
-		btn_down.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				if(cursor < contact_list.size()-1)
-					cursor++;
-				
-			    Contact contact = contact_list.getContact(cursor);
-			    output1.setText(contact.getPhoneNumber());
-			    output2.setText(contact.getGivenName());
-			    output3.setText(contact.getFamilyName());			
-			}});
-		
-		btn_up.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				if(cursor > 0)
-					cursor--;
-				
-				Contact contact = contact_list.getContact(cursor);
-			    output1.setText(contact.getPhoneNumber());
-			    output2.setText(contact.getGivenName());
-			    output3.setText(contact.getFamilyName());
-			}});
-				
-    }
+	int zero = 0;
+	int one = 0;
+	int two = 0;
+	int three = 0;
+	int four = 0;
+	int five = 0;
+	int six = 0;
+	int seven = 0;
+	int eight = 0;
+	int nine = 0;
+	
+  @Override
+  protected void onCreate(Bundle savedInstanceState) 
+  {
+      
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_nokia_phone);
     
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) 
-	{
-        super.onWindowFocusChanged(hasFocus);
+    context = this.getApplicationContext();
+    
+    sp = new SoundPool(99, AudioManager.STREAM_MUSIC, 0);
+    
+    zero = sp.load(this, R.raw.s0, 1);
+    one = sp.load(this, R.raw.s1, 1);
+    two = sp.load(this, R.raw.s2, 1);
+    three = sp.load(this, R.raw.s3, 1);
+    four = sp.load(this, R.raw.s4, 1);
+    five = sp.load(this, R.raw.s5, 1);
+    six = sp.load(this, R.raw.s6, 1);
+    seven = sp.load(this, R.raw.s7, 1);
+    eight = sp.load(this, R.raw.s8, 1);
+    nine = sp.load(this, R.raw.s9, 1);
+    
+    
+    Button btn_zero = (Button) this.findViewById(R.id.btn_zero);
+  	Button btn_one = (Button) this.findViewById(R.id.btn_one);
+  	Button btn_two = (Button) this.findViewById(R.id.btn_two);
+  	Button btn_three = (Button) this.findViewById(R.id.btn_three);
+  	Button btn_four = (Button) this.findViewById(R.id.btn_four);
+  	Button btn_five = (Button) this.findViewById(R.id.btn_five);
+  	Button btn_six = (Button) this.findViewById(R.id.btn_six);
+  	Button btn_seven = (Button) this.findViewById(R.id.btn_seven);
+  	Button btn_eight = (Button) this.findViewById(R.id.btn_eight);
+  	Button btn_nine = (Button) this.findViewById(R.id.btn_nine);
+  	
+  	Button btn_star = (Button) this.findViewById(R.id.btn_star);
+  	Button btn_pound = (Button) this.findViewById(R.id.btn_pound);
+  	Button btn_clear = (Button) this.findViewById(R.id.btn_clear);
+  	Button btn_enter = (Button) this.findViewById(R.id.btn_enter);
+  	Button btn_down = (Button) this.findViewById(R.id.btn_down);
+  	Button btn_up = (Button) this.findViewById(R.id.btn_up);
+  	
+  	btn_zero.setOnClickListener(this);
+  	btn_one.setOnClickListener(this);
+  	btn_two.setOnClickListener(this);
+  	btn_three.setOnClickListener(this);
+  	btn_four.setOnClickListener(this);
+  	btn_five.setOnClickListener(this);
+  	btn_six.setOnClickListener(this);
+  	btn_seven.setOnClickListener(this);
+  	btn_eight.setOnClickListener(this);
+  	btn_nine.setOnClickListener(this);
+  	
+  	btn_star.setOnClickListener(this);
+  	btn_pound.setOnClickListener(this);
+  	btn_clear.setOnClickListener(this);
+  	btn_enter.setOnClickListener(this);
+  	btn_down.setOnClickListener(this);
+  	btn_up.setOnClickListener(this);
+  	
+  	output1 = (TextView) this.findViewById(R.id.textView_output1);
+  	output2 = (TextView) this.findViewById(R.id.textView_output2);
+  	output3 = (TextView) this.findViewById(R.id.textView_output3);
+  
+  	
+  	//contact_list = new ContactList(context);
+  	
+ 
+  }
+  
 
-        if(hasFocus) 
-        {
-        	new LayoutScaler();
-			LayoutScaler.scaleContents(findViewById(R.id.contents), findViewById(R.id.container));
-        }
-    }
-    
-    private void digitButton(String digit){
-		phone_number += digit;
-		output1.setText(phone_number);
-		output2.setText("");
-		output3.setText("");
-	}
-    
-    private void call(String phone_number) 
-    {
-    	Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + output1.getText()));
+  
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) 
+  {
+      super.onWindowFocusChanged(hasFocus);
+
+      if(hasFocus) 
+      {
+      	new LayoutScaler();
+      	LayoutScaler.scaleContents(findViewById(R.id.contents), findViewById(R.id.container));
+      }
+  }
+  
+  
+  
+  private void digitButton(String digit)
+  {
+  	phone_number += digit;
+  	output1.setText(phone_number);
+  	output2.setText("");
+  	output3.setText("");
+  }
+  
+  private void call(String phone_number) 
+  {
+  	Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + output1.getText()));
     	startActivity(callIntent);
+  }
+
+
+
+  @Override
+  public void onClick(View v)
+  {
+    if (v.getId() == R.id.btn_zero)
+    {
+      digitButton("0");
+      sp.play(zero, 1, 1, 0, 0, 1);   
     }
+    
+    else if (v.getId() == R.id.btn_one)
+    {
+      digitButton("1");
+      sp.play(one, 1, 1, 0, 0, 1);   
+    }
+    
+    else if (v.getId() == R.id.btn_two)
+    {
+      digitButton("2");
+      sp.play(two, 1, 1, 0, 0, 1); 
+      
+    }
+    
+    else if (v.getId() == R.id.btn_three)
+    {
+      digitButton("3");
+      sp.play(three, 1, 1, 0, 0, 1); 
+    }
+    
+    else if (v.getId() == R.id.btn_four)
+    {
+      digitButton("4");
+      sp.play(four, 1, 1, 0, 0, 1); 
+    }
+    
+    else if (v.getId() == R.id.btn_five)
+    {
+      digitButton("5");
+      sp.play(five, 1, 1, 0, 0, 1); 
+      
+    }
+    
+    else if (v.getId() == R.id.btn_six)
+    {
+      digitButton("6");
+      sp.play(six, 1, 1, 0, 0, 1); 
+      
+    }
+    
+    else if (v.getId() == R.id.btn_seven)
+    {
+      digitButton("7");
+      sp.play(seven, 1, 1, 0, 0, 1); 
+    }
+    
+    else if (v.getId() == R.id.btn_eight)
+    {
+      digitButton("8");
+      sp.play(eight, 1, 1, 0, 0, 1); 
+    }
+    
+    else if (v.getId() == R.id.btn_nine)
+    {
+      digitButton("9");
+      sp.play(nine, 1, 1, 0, 0, 1); 
+    }
+    
+    else if (v.getId() == R.id.btn_star)
+    {
+    
+      
+    }
+    
+    else if (v.getId() == R.id.btn_pound)
+    {
+    
+      
+    }
+    
+    else if (v.getId() == R.id.btn_clear)
+    {
+      if(phone_number.length() > 0)
+      {
+        phone_number = phone_number.substring(0, phone_number.length() - 1);
+        output1.setText(phone_number);
+      }
+      
+    }
+    
+    else if (v.getId() == R.id.btn_enter)
+    {
+      call(phone_number);
+    }
+    else if (v.getId() == R.id.btn_down)
+    {
+      /*
+      if(cursor < contact_list.size()-1)
+        cursor++;
+        
+      Contact contact = contact_list.getContact(cursor);
+      output1.setText(contact.getPhoneNumber());
+      output2.setText(contact.getGivenName());
+      output3.setText(contact.getFamilyName());     
+       
+       */
+      
+    }
+    else if (v.getId() == R.id.btn_up)
+    {
+      /*
+      if(cursor > 0)
+        cursor--;
+      
+      Contact contact = contact_list.getContact(cursor);
+        output1.setText(contact.getPhoneNumber());
+        output2.setText(contact.getGivenName());
+        output3.setText(contact.getFamilyName());
+      */
+    }
+    
+    
+  }
 }
