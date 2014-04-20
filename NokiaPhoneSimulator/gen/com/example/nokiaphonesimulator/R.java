d5ec378707535f03f83f9d89b4a7244bb7b6cb6c
Thomas Paul <paul.xx9>
2014-04-20 10:53:21 +0200

    [VP, TP] deletion fix!

--- NokiaPhoneSimulator/gen/com/example/nokiaphonesimulator/R.java
+++ NokiaPhoneSimulator/gen/com/example/nokiaphonesimulator/R.java
@@ -63,28 +63,32 @@ public final class R {
         public static final int nokia_phone=0x7f020031;
     }
     public static final class id {
-        public static final int btn_clear=0x7f070003;
-        public static final int btn_down=0x7f070004;
-        public static final int btn_eight=0x7f07000e;
-        public static final int btn_enter=0x7f070005;
-        public static final int btn_five=0x7f07000b;
-        public static final int btn_four=0x7f07000a;
-        public static final int btn_nine=0x7f07000f;
-        public static final int btn_one=0x7f070007;
-        public static final int btn_pound=0x7f070012;
-        public static final int btn_seven=0x7f07000d;
-        public static final int btn_six=0x7f07000c;
-        public static final int btn_star=0x7f070010;
-        public static final int btn_three=0x7f070009;
-        public static final int btn_two=0x7f070008;
-        public static final int btn_up=0x7f070006;
-        public static final int btn_zero=0x7f070011;
-        public static final int container=0x7f070000;
-        public static final int contents=0x7f070001;
-        public static final int textView_output1=0x7f070002;
+        public static final int action_settings=0x7f080013;
+        public static final int btn_clear=0x7f080003;
+        public static final int btn_down=0x7f080004;
+        public static final int btn_eight=0x7f08000e;
+        public static final int btn_enter=0x7f080005;
+        public static final int btn_five=0x7f08000b;
+        public static final int btn_four=0x7f08000a;
+        public static final int btn_nine=0x7f08000f;
+        public static final int btn_one=0x7f080007;
+        public static final int btn_pound=0x7f080012;
+        public static final int btn_seven=0x7f08000d;
+        public static final int btn_six=0x7f08000c;
+        public static final int btn_star=0x7f080010;
+        public static final int btn_three=0x7f080009;
+        public static final int btn_two=0x7f080008;
+        public static final int btn_up=0x7f080006;
+        public static final int btn_zero=0x7f080011;
+        public static final int container=0x7f080000;
+        public static final int contents=0x7f080001;
+        public static final int textView_output1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_nokia_phone=0x7f030000;
+    }
+    public static final class menu {
+        public static final int nokia_phone=0x7f070000;
     }
     public static final class raw {
         public static final int s0=0x7f040000;
@@ -100,8 +104,9 @@ public final class R {
         public static final int tastenton=0x7f04000a;
     }
     public static final class string {
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int menu=0x7f050001;
+        public static final int menu=0x7f050002;
     }
     public static final class style {
         /** 
